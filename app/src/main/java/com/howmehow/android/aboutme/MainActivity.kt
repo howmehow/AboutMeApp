10b6e16f51726c30c06022521c15a6bfa4982411
howmehow <howmehow@gmail.com>
2020-05-27 10:37:38 +0100

    Adding data class for easier store of data, not in @string, but rather in new class

--- app/src/main/java/com/howmehow/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/howmehow/android/aboutme/MainActivity.kt
@@ -14,11 +14,13 @@ import com.howmehow.android.aboutme.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("Michal")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName = myName
 //        findViewById<Button>(R.id.done_button).setOnClickListener {
 //            addNickname(it)
 //        }
---
+++ app/src/main/java/com/howmehow/android/aboutme/MyName.kt
@@ -0,0 +1,3 @@
+package com.howmehow.android.aboutme
+
+data class MyName(var name: String="", var nickname: String="")
